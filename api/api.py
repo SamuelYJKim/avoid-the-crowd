16d5a4afd70ea228ceb7adb693a33be4f84dc0e8
Ahli <atalefard@gmail.com>
2020-08-16 21:55:46 -0700

    added comment

--- api/api.py
+++ api/api.py
@@ -8,7 +8,7 @@ from scripts import authenticate
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-
+# given latitute, longitutde and keyword, find places
 @app.route('/places/<latitude>/<longitude>/<keyword>')
 def get_places(latitude, longitude, keyword):
     return places(latitude, longitude, keyword)
